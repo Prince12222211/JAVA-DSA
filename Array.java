c2ef4e7463aa1f0f1c7dc6bf13fb1449b245e498
Prince5047 <princeverma9504@gmail.com>
2024-08-25 18:56:11 +0530

    new files are added

--- Array.java
+++ Array.java
@@ -1,5 +1,31 @@
-public class Array {
-    public static void main(String[] args) {
+import java.sql.SQLOutput;
+import java.util.Arrays;
+import java.util.Scanner;
 
-    }
-}
+//public class Array {
+//    public static void main(String[] args) {
+//        //Store a roll number
+//        //Syntax
+//        //datatype[]variable_name=new datatype[size];
+//        //store 5 roll number;
+//
+////        int []rnos=new int[5];
+//        //or directly
+////        int []rnos2={23,21,45,32,15};
+////        int []ros; //declaration of the array. ros is getting defined in the stack
+////        ros=new int[5]; //initialistation: actually here object is being created in the memory (heap)
+////        System.out.println(rnos2.toString());
+//        Scanner scan=new Scanner(System.in);
+//        int n=scan.nextInt();
+//
+//        int arr[]=new int[n];
+//        for(int i=0;i<n;i++){
+//            arr[i]=scan.nextInt();
+//        }
+//        for(int i=0;i<=n;i++){
+//            System.out.println(Arrays.toString(arr));
+//        }
+//    }
+//}
+
+
