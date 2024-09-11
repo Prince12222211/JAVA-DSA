6475d42ce4726cd50a2ecde1480efcbc638ed07c
Prince5047 <princeverma9504@gmail.com>
2024-09-11 19:57:42 +0530

    new codes are added

--- Array.java
+++ Array.java
@@ -2,6 +2,7 @@ import java.sql.SQLOutput;
 import java.util.Arrays;
 import java.util.Scanner;
 
+
 //public class Array {
 //    public static void main(String[] args) {
 //        //Store a roll number
@@ -17,15 +18,105 @@ import java.util.Scanner;
 ////        System.out.println(rnos2.toString());
 //        Scanner scan=new Scanner(System.in);
 //        int n=scan.nextInt();
-//
+////
 //        int arr[]=new int[n];
-//        for(int i=0;i<n;i++){
+//        for(int i=0;i<=n;i++){
 //            arr[i]=scan.nextInt();
 //        }
 //        for(int i=0;i<=n;i++){
 //            System.out.println(Arrays.toString(arr));
 //        }
+//
+//
 //    }
 //}
 
 
+//public class Array
+//{
+//    public  static  void main(String args[]){
+//        Scanner scan=new Scanner(System.in);
+////        int n=scan.nextInt();
+//        int []nums={3,4,5,12};
+//        System.out.println(Arrays.toString(nums));
+//        change(nums);
+//        System.out.println(Arrays.toString(nums));
+//    }
+//    static  void change(int []arr){
+//        arr[0]=99;
+//    }
+//}
+
+
+//Multidimensional Array
+//public  class Array {
+//    public static  void main(String [] args){
+//        Scanner scan=new Scanner(System.in);
+//        int n=scan.nextInt();
+//        int m=scan.nextInt();
+//        int arr[][]=new int[n][m];
+//        for(int i=0;i<n;i++){
+//            for(int j=0;j<m;j++){
+//                arr[i][j]=scan.nextInt();
+//            }
+//        }
+//        for(int i=0;i<n;i++){
+//            for(int j=0;j<m;j++){
+//                System.out.print(arr[i][j]+" ");
+//            }
+//            System.out.println();
+//        }
+//    }
+//}
+public  class Array
+{
+    public  static  void main(String args[]){
+        Scanner scan=new Scanner(System.in);
+        int [][]arr=new int[3][3];
+
+        System.out.println(arr.length); //no. of rows
+        for(int row=0;row<arr.length;row++){
+            for(int col=0;col<arr[row].length;col++){
+                arr[row][col]=scan.nextInt();
+
+            }
+        }
+        for(int row=0;row<arr.length;row++){
+            for(int col=0;col<arr[row].length;col++){
+//                arr[row][col]=scan.nextInt();
+
+                System.out.print(arr[row][col]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
+//output:
+//1 2 3
+// 4 5 6
+// 7 8 9
+
+
+//To print the array in the other method
+//public  class Array
+//{
+//    public  static  void main(String args[]){
+//        Scanner scan=new Scanner(System.in);
+//        int [][]arr=new int[3][3];
+//
+//        System.out.println(arr.length); //no. of rows
+//        for(int row=0;row<arr.length;row++){
+//            for(int col=0;col<arr[row].length;col++){
+//                arr[row][col]=scan.nextInt();
+//
+//            }
+//        }
+//        for(int row=0;row<arr.length;row++) {
+//            System.out.println(Arrays.toString(arr[row]));
+//        }
+//    }
+//}
+//output:
+//[1, 2, 3]
+//[4, 5, 6]
+//[7, 8, 9]
