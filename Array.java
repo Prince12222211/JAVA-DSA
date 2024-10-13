daf8ebec4ebb89f14645ab6ab60979283160b6c5
Prince5047 <princeverma9504@gmail.com>
2024-10-13 21:20:14 +0530

    new codes are added

--- Array.java
+++ Array.java
@@ -302,50 +302,51 @@ import java.util.Scanner;
 //    }
 //}
 
-
-public  class Array {
-    public static void main(String[] args) {
-        Scanner scan=new Scanner(System.in);
-      int n=scan.nextInt();
-        int arr[]=new int[n];
-        for(int i=0;i<n;i++){
-    arr[i]=scan.nextInt();
-        }
-        int target=scan.nextInt();
-        int ans=orderAgnosticBS(arr,target);
-        System.out.println("The element is present in the no of the index at: "+ans);
-    }
-    static  int orderAgnosticBS(int arr[],int target){
-        //find wheter the array is sorted in ascending or descending
-
-        int start=0;
-        int end=arr.length-1;
-        boolean isAsc=arr[start]<arr[end];
-        while(start<=end){
-            int mid=start+(end-start)/2;
-            if(arr[mid]==target){
-                return  mid;
-            }
-            if(isAsc) {
-                if(target<arr[mid]){
-                    end=mid-1;
-
-                }
-                else{
-                    start=mid+1;
-                }
-            }
-            else{
-                if(target>arr[mid]){
-                    end=mid-1;
-                }
-                else{
-                    start=mid+1;
-                }
-            }
-            }
-
-        return  -1;
-
-    }
+//
+//public  class Array {
+//    public static void main(String[] args) {
+//        Scanner scan=new Scanner(System.in);
+//      int n=scan.nextInt();
+//        int arr[]=new int[n];
+//        for(int i=0;i<n;i++){
+//    arr[i]=scan.nextInt();
+//        }
+//        int target=scan.nextInt();
+//        int ans=orderAgnosticBS(arr,target);
+//        System.out.println("The element is present in the no of the index at: "+ans);
+//    }
+//    static  int orderAgnosticBS(int arr[],int target){
+//        //find wheter the array is sorted in ascending or descending
+//
+//        int start=0;
+//        int end=arr.length-1;
+//        boolean isAsc=arr[start]<arr[end];
+//        while(start<=end){
+//            int mid=start+(end-start)/2;
+//            if(arr[mid]==target){
+//                return  mid;
+//            }
+//            if(isAsc) {
+//                if(target<arr[mid]){
+//                    end=mid-1;
+//
+//                }
+//                else{
+//                    start=mid+1;
+//                }
+//            }
+//            else{
+//                if(target>arr[mid]){
+//                    end=mid-1;
+//                }
+//                else{
+//                    start=mid+1;
+//                }
+//            }
+//            }
+//
+//        return  -1;
+//
+//    }
 }
+
---
+++ leetcode.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class leetcode {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+
+    }
+}
