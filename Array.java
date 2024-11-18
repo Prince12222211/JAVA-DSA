858a072d037598eda87413fe01199afe0a888bc9
Prince5047 <114815472+Prince12222211@users.noreply.github.com>
2024-11-18 18:34:56 +0530

    Update Array.java

--- Array.java
+++ Array.java
@@ -2,11 +2,6 @@ import java.sql.SQLOutput;
 import java.util.Arrays;
 import java.util.Scanner;
 import java.util.Scanner;
-
-
-
-
-
 //public class Array {
 //    public static void main(String[] args) {
 //        //Store a roll number
